5533c3c64a9376b66aa14aa7807a3752bdb7dde1
Sarthak Arora <sarthakarora1503@gmail.com>
2022-06-11 21:16:50 +0530

    first question of the day

---
+++ Leetcode/Easy/476.number-complement.cpp
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode id=476 lang=cpp
+ *
+ * [476] Number Complement
+ *
+ * https://leetcode.com/problems/number-complement/description/
+ *
+ * algorithms
+ * Easy (66.79%)
+ * Likes:    2034
+ * Dislikes: 106
+ * Total Accepted:    267.1K
+ * Total Submissions: 399.5K
+ * Testcase Example:  '5'
+ *
+ * The complement of an integer is the integer you get when you flip all the
+ * 0's to 1's and all the 1's to 0's in its binary representation.
+ * 
+ * 
+ * For example, The integer 5 is "101" in binary and its complement is "010"
+ * which is the integer 2.
+ * 
+ * 
+ * Given an integer num, return its complement.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: num = 5
+ * Output: 2
+ * Explanation: The binary representation of 5 is 101 (no leading zero bits),
+ * and its complement is 010. So you need to output 2.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: num = 1
+ * Output: 0
+ * Explanation: The binary representation of 1 is 1 (no leading zero bits), and
+ * its complement is 0. So you need to output 0.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= num < 2^31
+ * 
+ * 
+ * 
+ * Note: This question is the same as 1009:
+ * https://leetcode.com/problems/complement-of-base-10-integer/
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findComplement(int num) {
+        long ans = 0;
+        int c = 1;
+
+        while(num)
+        {
+            ans += c*((num&1) ^ 1);
+            num >>= 1;
+            c <<= 1;
+        }
+
+        return ans;
+    }
+};
+// @lc code=end
+
