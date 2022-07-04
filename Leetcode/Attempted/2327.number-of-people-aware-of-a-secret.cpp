2a89bf3a4efee698d1b99153100b21061d53ccf5
Sarthak Arora <sarthakarora1503@gmail.com>
2022-07-05 03:36:31 +0530

    attempted but incomplete

---
+++ Leetcode/Attempted/2327.number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,15 @@
+
+// * @lc app=leetcode id=2327 lang=cpp
+
+// @lc code=start
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        
+    }
+};
+
+// @lc code=end
---
+++ Leetcode/Attempted/2328.number-of-increasing-paths-in-a-grid.cpp
@@ -0,0 +1,15 @@
+
+// * @lc app=leetcode id=2328 lang=cpp
+
+// @lc code=start
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countPaths(vector<vector<int>>& grid) {
+        
+    }
+};
+
+// @lc code=end
